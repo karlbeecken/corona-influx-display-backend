5f9453861e869deba94556b2eec99ad4fd5feedd
Karl Beecken <karl@beecken.berlin>
2021-06-10 16:17:48 +0200

    👽️ fix loop, again

--- data.ts
+++ data.ts
@@ -71,7 +71,7 @@ async function getIncidence(): Promise<object> {
         },
       };
 
-      for (let i = res.data.index.length - 7; i < res.data.index.length; i++) {
+      for (let i = 0; i < 7; i++) {
         let count =
           parseInt(res.data.index[i].mitte) +
           parseInt(res.data.index[i].friedrichshain_kreuzberg) +
